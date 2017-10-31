fd26ff0af2470b15464420e3b8306c8a202ff9f2
Andrea Zucchini <azga84@gmail.com>
2017-10-31 17:08:39 +0100

    use auth handler

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	flag.Parse()
 
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	go r.run()
 
