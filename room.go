04b7df7090cf35ea717c245d34fa0e2d876e1ec6
Andrea Zucchini <azga84@gmail.com>
2017-10-31 16:26:03 +0100

    modeling a chat room

---
+++ room.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+	"github.com/zucchinidev/go-chat-application/trace"
+	"os"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+type room struct {
+	// forward is a channel that holds incoming messages
+	// that should be forwarded to the other clients
+	forward chan []byte
+	// join is a channel for clients wishing to join the room
+	join chan *client
+	// leave is a channel for clients wishing to leave the room
+	leave chan *client
+	// clients holds all currents clients in this room.
+	clients map[*client]bool
+	// tracer will receive trace information of activity  in the room.
+	tracer trace.Tracer
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("New client joined")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("Client left room")
+		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", string(msg))
+			for client := range r.clients {
+				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
+}
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(res, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP", err)
+		return
+	}
+
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	//  call the read method in the main thread, which will block operations (keeping the connection alive) until it's time to close it.
+	client.read()
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer: trace.New(os.Stdout),
+	}
+}
