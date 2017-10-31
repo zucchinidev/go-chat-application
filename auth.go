8ff967575c434f4b7eccf4f1cd2436d1e142514b
Andrea Zucchini <azga84@gmail.com>
2017-10-31 17:08:25 +0100

    create an authorization wrapper handler that perform the check and pass the execution on the inner handler

---
+++ auth.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	_, err := req.Cookie("auth")
+	if err == http.ErrNoCookie {
+		res.Header().Set("Location", "/login")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	h.next.ServeHTTP(res, req)
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
